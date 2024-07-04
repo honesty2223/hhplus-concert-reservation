e21770dfcb78211873eb9e4f6e3f1e9f17172239
honesty2223 <honesty220203@gmail.com>
2024-07-04 19:34:44 +0900

    init

---
+++ src/test/java/hhplus/concert/reservation/HhplusConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.concert.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConcertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
