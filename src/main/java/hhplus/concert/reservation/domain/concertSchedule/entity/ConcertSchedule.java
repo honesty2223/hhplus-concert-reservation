cfd28d3ea9a379e4342544bb8ed732963ac503a1
honesty2223 <honesty220203@gmail.com>
2024-08-02 08:23:23 +0900

    Redis 캐싱 비지니스 로직 수정

--- src/main/java/hhplus/concert/reservation/domain/concertSchedule/entity/ConcertSchedule.java
+++ src/main/java/hhplus/concert/reservation/domain/concertSchedule/entity/ConcertSchedule.java
@@ -1,5 +1,11 @@
 package hhplus.concert.reservation.domain.concertSchedule.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,13 +34,19 @@ public class ConcertSchedule {
     @Column(name = "seat_count")
     private long seatCount;
 
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     @Column(name = "concert_date")
     private LocalDate concertDate;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
