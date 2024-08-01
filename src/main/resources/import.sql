cbb01493c761e2d93ee6210f66359b70e9ef9c9c
honesty2223 <honesty220203@gmail.com>
2024-08-02 08:21:22 +0900

    Redis 기초 세팅

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+
+-- 콘서트 데이터
+INSERT INTO concert (concert_name) VALUES ('이무진 콘서트');
+INSERT INTO concert (concert_name) VALUES ('소수빈 콘서트');
+INSERT INTO concert (concert_name) VALUES ('뉴진스 콘서트');
+
+
+-- 콘서트 스케줄 데이터
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (1, 50, '2024-08-30');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (1, 50, '2024-09-06');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (1, 50, '2024-09-13');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (2, 50, '2024-08-31');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (2, 50, '2024-09-07');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (2, 50, '2024-09-14');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (3, 50, '2024-09-01');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (3, 50, '2024-09-08');
+INSERT INTO concert_schedule (concert_id, seat_count, concert_date) VALUES (3, 50, '2024-09-15');
