6b5d63f6187b4046baabdaddc755e2083dcb45e9
honesty2223 <honesty220203@gmail.com>
2024-08-16 06:40:45 +0900

    패키지 리팩토링

--- src/main/java/hhplus/concert/reservation/infrastructure/ConcertJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package hhplus.concert.reservation.infrastructure;
-
-import hhplus.concert.reservation.domain.concert.entity.Concert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConcertJpaRepository extends JpaRepository<Concert, Long> {
-}
